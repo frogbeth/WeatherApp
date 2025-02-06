6dd93f7b4c6ba145e5e823cd23ef6ed666d2f9cf
Elizabeth Castaneda <girlygirl12375@gmail.com>
2025-02-06 14:07:26 -0600

    add code

--- scripts.js
+++ scripts.js
@@ -1,22 +1,23 @@
-fetch('https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/oklahoma?unitGroup=metric&key=XEJKBQRJ2QVRHHZ45N34CGYHA&contentType=json')
-    .then(function (response) {
-        return response.json();
-    })
-    .then(function (response) {
-        console.log(response);
-    }); // lines 1-7 successfully fetch the weather info for oklahoma and log to the console.
-
-async function fetchData(){ //reads input from the location search bar and logs json info to the console
-    const location = document.getElementById('locationSearch').value;
+async function fetchData() { //reads input from the location search bar and logs json info to the console
+  const location = document.getElementById('locationSearch').value;
   try {
     await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?unitGroup=metric&key=XEJKBQRJ2QVRHHZ45N34CGYHA&contentType=json`)
-    .then(function (response) {
+      .then(function (response) {
         return response.json();
-    })
-    .then(function (response) {
+      })
+      .then(function (response) {
         console.log(response);
-    });
+      })
+
+    if (!response.ok) {
+      throw new Error('Failed to fetch weather data');
+    }
+
+    const data = JSON.parse(response);
+    document.getElementById("location").innerHTML = data.address;
+
+
   } catch (error) {
-    return;
+    document.getElementById('weatherDisplay').innerHTML = 'Failed to load weather data.';
   }
 }
