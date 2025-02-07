dc44a6b3f8d1dea86e1a20aa643438b84f418a65
Elizabeth Castaneda <girlygirl12375@gmail.com>
2025-02-06 23:27:56 -0600

    css changes

--- scripts.js
+++ scripts.js
@@ -1,24 +1,15 @@
 async function fetchData() { //reads input from the location search bar and logs json info to the console
   const location = document.getElementById('locationSearch').value;
   try {
-    await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?unitGroup=metric&key=XEJKBQRJ2QVRHHZ45N34CGYHA&contentType=json`)
+    const response =await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?unitGroup=metric&key=XEJKBQRJ2QVRHHZ45N34CGYHA&contentType=json`)
       .then(function (response) {
         return response.json();
       })
       .then(function (response) {
         console.log(response);
       })
-      // .then(response => {
-      //   const resolvedAddress = response.resolvedAddress;
-      //   const description = response.currentConditions.conditions;
-      //   const temperature = response.currentConditions.temp;
-      //   console.log(resolvedAddress);
-      //   console.log(description);
-    
-      //   document.getElementById('location').textContent = resolvedAddress;
-      //   document.getElementById('description').textContent = description;
-      //   document.getElementById('temperature').textContent = `${temperature}°F`;
-      // })
+
+      document.getElementById("location").innerHTML = `${location}`;
 
     if (!response.ok) {
       throw new Error('Failed to fetch weather data');
